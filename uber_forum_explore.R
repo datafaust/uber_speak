8c17cc1e4973fd42e60c059ea658064c55d1af2b
datafox <faustolopez110@gmail.com>
2017-04-07 17:11:24 -0400

    adding more

--- uber_forum_explore.R
+++ uber_forum_explore.R
@@ -141,18 +141,34 @@ for (i in threads[, 2]) {
 names(master_forum) = c("uber_name","uber_write","uber_date", "page_num","threads","links")
 master_forum = master_forum[!duplicated(paste(uber_name, uber_write)),]
 master_forum[,tot_pop:= .N, by = .(uber_name)]
-
-
 master_forum[, pop_per_thread:= .N, by = .(uber_name, threads)]
-
-
-
 master_forum[,pop_per_thread:=sum(pop_per_thread), by = .(threads)]
-
-
 
 #lets turn all the letters in the character vectors into lowercase
 master_forum$uber_write = as.character(master_forum$uber_write)
+
+#remove any names referenced in comments
+master_forum$uber_write
+
+
+lapply(as.character(master_forum$uber_name), function(x){gsub(x, "", master_forum$uber_write)})
+
+a = paste(master_forum$uber_name, collapse = ",")
+cc <- paste(master_forum$uber_write, collapse = ",")
+for(i in seq_along(a)) cc <- gsub(a[i], "", cc, fixed = TRUE)
+
+
+lapply(1:nrow(master_forum), function(x) {
+  pattern = master_forum[x,1]
+  toclean = master_forum[x,2]
+  print(pattern)
+  print(toclean)
+  z = gsub(pattern, "", toclean)
+  print(z)
+})
+
+
+
 #master_forum$uber_write2 = gsub("[^[:alnum:] ]", "", master_forum$uber_write)
 write.csv(master_forum, "uber_forum.csv")
 
